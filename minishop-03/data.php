6f56cb7185030723affba822f6d488a313aa03d3
tranviet2160 <tranducviet21604@gmail.com>
2026-07-22 08:57:27 +0100

    Update data.php

--- minishop-03/data.php
+++ minishop-03/data.php
@@ -14,14 +14,14 @@ $categoryObjects = [
 
 /** @var Product[] $productObjects */
 $productObjects = [
-    new Product('KB-01', 'Keychron K2', 1, 1890000, 3),
-    new Product('KB-02', 'Akko 3087', 1, 1290000, 5),
+    new Product('KB-01', 'Keychron K2',    1, 1890000, 3),
+    new Product('KB-02', 'Akko 3087',      1, 1290000, 5),
     new Product('KB-03', 'Leopold FC660M', 1, 2750000, 2),
-    new Product('MS-01', 'Logitech M331', 2, 290000, 10),
-    new Product('MS-02', 'Razer Viper', 2, 990000, 4),
-    new Product('MS-03', 'Xiaomi Silent', 2, 250000, 8),
-    new Product('MN-01', 'Dell 24 inch', 3, 3200000, 2),
-    new Product('MN-02', 'LG UltraFine', 3, 8500000, 1),
+    new Product('MS-01', 'Logitech M331',  2, 290000,  10),
+    new Product('MS-02', 'Razer Viper',    2, 990000,  4),
+    new Product('MS-03', 'Xiaomi Silent',  2, 250000,  8),
+    new Product('MN-01', 'Dell 24 inch',   3, 3200000, 2),
+    new Product('MN-02', 'LG UltraFine',   3, 8500000, 1),
 ];
 
 /** Map id → tên danh mục (phục vụ in bảng) */
